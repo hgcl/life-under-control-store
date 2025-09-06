91f7093c9d4a0d74de4ddcec7a745b52518bec43
Clara Le <hey@clarale.com>
2025-09-06 17:28:46 +0200

    feat: create `<Header>` with working user sign-in, and a placeholder search bar

---
+++ components/Header.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { ClerkLoaded, SignInButton, UserButton, useUser } from "@clerk/nextjs";
+import Form from "next/form";
+import { TrolleyIcon } from "@sanity/icons";
+
+function Header() {
+  const { user } = useUser();
+  return (
+    <header>
+      <div>
+        <a href="/">Life Under Control</a>
+
+        {/* SEARCH */}
+        {/* TODO: review accessibility of this search form section */}
+        {/* TODO: make search work with working data */}
+        <Form action="/search">
+          <input name="query" type="text" placeholder="Search for products" />
+          <button type="submit">Submit</button>
+        </Form>
+
+        {/* BASKET */}
+        <a href="/basket">
+          <TrolleyIcon />
+          {/* TODO: add item count */}
+          <span>Basket</span>
+        </a>
+
+        {/* USER ACCOUNT */}
+        {/* Only enable if Clerk is loaded */}
+        <ClerkLoaded>
+          {/* If user exists... */}
+          {user && (
+            <a href="/orders">
+              <span>Orders</span>
+            </a>
+          )}
+
+          {user ? <UserButton /> : <SignInButton mode="modal" />}
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
