94b1869fdb11c7d5abdbe82a43283de331f87d32
Clara Le <hey@clarale.com>
2025-10-01 16:39:46 +0200

    fix: fix "no-unused-vars" error by ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,16 +10,33 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+  ...compat.config(
+    {
+      extends: ["next/core-web-vitals", "next/typescript"],
+    },
+    {
+      rules: {
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            args: "all",
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+          },
+        ],
+      },
+    },
+    {
+      ignores: [
+        "node_modules/**",
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+      ],
+    }
+  ),
 ];
 
 export default eslintConfig;
