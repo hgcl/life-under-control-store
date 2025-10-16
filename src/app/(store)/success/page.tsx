d99622a4a24f7ff35b69fa154f24b40bea856544
Clara Le <hey@clarale.com>
2025-10-16 12:31:26 +0200

    style: style Success page

--- src/app/(store)/success/page.tsx
+++ src/app/(store)/success/page.tsx
@@ -4,6 +4,8 @@ import { CartContext } from "@/src/context/CartContextProvider";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useContext, useEffect } from "react";
+import styles from "./page.module.css";
+import Button from "@/src/components/Button";
 
 const SuccessPage = () => {
   // In `createCheckoutSession.ts`, the order number was saved as a URL param on success
@@ -19,15 +21,21 @@ const SuccessPage = () => {
   }, [orderNumber, clearCart]);
 
   return (
-    <div>
-      <p>Thank you for your order!</p>
-      <p>Order number: {orderNumber}</p>
-      <p>
-        A confirmation email has been sent to your registered email address.
-      </p>
-      <Link href="/orders">View order details</Link>
-      <Link href="/">Continue shopping</Link>
-    </div>
+    <section className={styles.Success}>
+      <h2>Thank you for your order!</h2>
+      <div className={styles.Success_content}>
+        <p>
+          Order number:{" "}
+          <span className={styles.Success_code}>{orderNumber}</span>
+        </p>
+      </div>
+      <div className={styles.Success_buttons}>
+        <Button href="/orders" type="primary">
+          View order details
+        </Button>
+        <Button href="/">Continue shopping</Button>
+      </div>
+    </section>
   );
 };
 
