68e469bc793ef2dff845e840f42eb24207de3eb7
Clara Le <hey@clarale.com>
2025-10-21 19:30:18 +0200

    style: draft styling for order page

--- src/components/OrderItem.tsx
+++ src/components/OrderItem.tsx
@@ -6,40 +6,51 @@ import Link from "next/link";
 import styles from "./OrderItem.module.css";
 
 const OrderItem = ({ order }: { order: MY_ORDERS_QUERYResult[0] }) => (
-  <article>
-    <h2>Order number {order.orderNumber}</h2>
-    <p>
-      Order date{" "}
-      {order.orderDate ? new Date(order.orderDate).toLocaleDateString() : "N/A"}
-    </p>
-    <p>Total amount {formatCurrency(order.totalPrice ?? 0, order.currency)}</p>
-    <p>Status {order.status}</p>
-    <section className="order-items">
-      <h3>Order items</h3>
-      <ul className={styles.Order_list}>
+  <article className={styles.Order_card}>
+    <section>
+      <h2>Order {order.orderNumber}</h2>
+      <p>
+        Purchased on{" "}
+        {order.orderDate
+          ? new Date(order.orderDate).toLocaleDateString()
+          : "N/A"}
+      </p>
+      <p>
+        Total amount {formatCurrency(order.totalPrice ?? 0, order.currency)}
+      </p>
+      <p>Status {order.status}</p>
+    </section>
+    <section className={styles.Order_list}>
+      <h3>Item(s) purchased</h3>
+      <ul>
         {order.products &&
           order.products.map((product) => (
-            <li key={product._id}>
+            <li key={product._id} className={styles.Order_item}>
               {product.image && (
-                <div className="img">
+                <div className={styles.Order_itemImage}>
                   <Image
                     src={`${imageUrl(product.image).url()}`}
-                    width={400}
-                    height={300}
+                    width={100}
+                    height={100}
                     alt=""
                   />
                 </div>
               )}
-              <p>
-                {product.archived ? (
-                  product.name + ` (archived listing)`
-                ) : (
-                  <Link href={`/product/${product.slug?.current}`}>
-                    {product.name}
-                  </Link>
-                )}
-                 {formatCurrency(product.price ?? 0, "eur")}
-              </p>
+              <div className={styles.Order_itemDescription}>
+                <p>
+                  {product.archived ? (
+                    product.name + ` (archived listing)`
+                  ) : (
+                    <Link
+                      className={styles.Order_itemLink}
+                      href={`/product/${product.slug?.current}`}
+                    >
+                      {product.name}
+                    </Link>
+                  )}
+                </p>
+                <p>Price {formatCurrency(product.price ?? 0, "eur")}</p>
+              </div>
             </li>
           ))}
       </ul>
