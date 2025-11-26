cbb46dce47549fe3e56aaca917097266049dc36b
Clara Le <hey@clarale.com>
2025-11-26 17:00:02 +0100

    fix: support icon names with dashes

--- src/components/FeatherIcon/FeatherIcon.tsx
+++ src/components/FeatherIcon/FeatherIcon.tsx
@@ -1,12 +1,17 @@
 import * as Icons from "react-feather";
 // import type Icon as IconType from "react-feather";
 
-function capitalizeFirstLetter(string: string) {
+function formatName(string: string) {
+  // From `something-like-this` to `somethingLikeThis`
+  string = string
+    .toLowerCase()
+    .replace(/-(.)/g, (match, group) => group.toUpperCase());
+  // Capitalize first letter
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 const FeatherIcon = ({ name }: { name: string }) => {
-  const key = capitalizeFirstLetter(name);
+  const key = formatName(name);
   const Icon = (Icons as Record<string, React.ComponentType>)[key];
 
   return Icon ? <Icon /> : <Icons.AlertTriangle />;
